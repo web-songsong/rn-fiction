1a34bb18eca9721f878cd0a43b81d78270ab2e64
charmingsong <web_songsong@163.com>
2019-11-27 19:57:47 +0800

    home dev

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,93 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
+// import React from 'react';
+// import {Text, View} from 'react-native';
+// import {createAppContainer} from 'react-navigation';
+// import {createBottomTabNavigator} from 'react-navigation-tabs';
+//
+// class HomeScreen extends React.Component {
+//   render() {
+//     return (
+//       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//         <Text>Home!</Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// class SettingsScreen extends React.Component {
+//   render() {
+//     return (
+//       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//         <Text>Settings!</Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const TabNavigator = createBottomTabNavigator({
+//   Home: HomeScreen,
+//   Settings: SettingsScreen,
+// });
+//
+// export default createAppContainer(TabNavigator);
+
+// import React from 'react';
+// import {createAppContainer} from 'react-navigation';
+// import {AppRegistry} from 'react-native';
+//
+// import {createStackNavigator} from 'react-navigation-stack';
+// import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+import Test from './test';
 import Home from './pages/Home';
-export default () => {
-  return (
-      <Home />
-  );
-};
+
+// // const MainNavigator = createStackNavigator({
+// //   Home: {screen: Home},
+// //   Profile: {screen: Test},
+// // });
+//
+// const TabNavigator = createBottomTabNavigator({
+//   Home: {screen: Home},
+//   Profile: {screen: Test},
+// });
+// export default createAppContainer(TabNavigator);
+import * as React from 'react';
+import {View, StyleSheet, Dimensions} from 'react-native';
+import {TabView, SceneMap} from 'react-native-tab-view';
+
+const FirstRoute = () => (
+  <View style={[styles.scene, {backgroundColor: '#ff4081'}]} />
+);
+
+const SecondRoute = () => (
+  <View style={[styles.scene, {backgroundColor: '#673ab7'}]} />
+);
+
+export default class TabViewExample extends React.Component {
+  state = {
+    index: 1,
+    routes: [{key: 'home', title: 'First'}, {key: 'second', title: 'Second'}],
+  };
+
+  render() {
+    return (
+      <TabView
+        navigationState={this.state}
+        renderScene={SceneMap({
+          second: SecondRoute,
+          home: Home,
+        })}
+        onIndexChange={index => {
+          console.log('xxx', index);
+          this.setState({index});
+        }}
+        // initialLayout={{width:200}}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+  },
+});
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import SearchBarHome from '../../components/SearchBarHome';
 import Headerdiscover from '../../components/Headerdiscover';
-import {View} from 'react-native';
+import {View, Button} from 'react-native';
+
+// interface PropsNavigate {
+//   screenProps: any;
+//   navigation: any;
+// }
 
 const Home = () => {
+  // const {navigate} = navigation;
   return (
     <>
       <View style={{backgroundColor: 'rgb(26,26,26)'}}>
         <Headerdiscover />
       </View>
       <SearchBarHome />
+      {/*<Button*/}
+      {/*  title="Go to Jane's profile"*/}
+      {/*  onPress={() => navigate('Profile', {name: 'Jane'})}*/}
+      {/*/>*/}
     </>
   );
 };
