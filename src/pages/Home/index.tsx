f554f43f449ceb2854eb6f8c8aea53d75e9f0903
charmingsong <web_songsong@163.com>
2019-11-27 16:28:54 +0800

    icons init

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,36 +1,13 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
-import {List, InputItem, Button} from 'antd-mobile-rn';
-
+import {Button} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 const Home = () => {
   return (
-    <ScrollView>
-      <List renderHeader={'node'}>
-        <InputItem
-          onChange={value => {
-            console.log(value);
-          }}
-          placeholder="url">
-          url:
-        </InputItem>
-        <InputItem
-          onChange={value => {
-            console.log(value);
-          }}
-          placeholder="keyword">
-          key:
-        </InputItem>
-        <List.Item>
-          <Button
-            onClick={() => {
-              console.log(fetch);
-            }}
-            type="primary">
-            下载
-          </Button>
-        </List.Item>
-      </List>
-    </ScrollView>
+    <Button
+      icon={<Icon name="arrow-right" size={15} color="white" />}
+      iconRight
+      title="Button with right icon"
+    />
   );
 };
 
