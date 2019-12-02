3dba3be5fccfc083f0626d430bef04db009f88e2
charmingsong <web_songsong@163.com>
2019-12-02 20:06:45 +0800

    router tabs  init

--- src/components/CarouselImg.tsx
+++ src/components/CarouselImg.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Image} from 'react-native-elements';
 import Swiper from 'react-native-swiper';
-import {View} from 'react-native';
-interface ImgItemType {
+import AnimatedTabs from 'react-native-animated-tabs';
+import {View, TouchableOpacity} from 'react-native';
+
+export interface ImgItemType {
   id: number;
   uri: string;
 }
 interface CarouselProps {
   dataList: Array<ImgItemType>;
+  onPress: Function;
 }
-
-export default ({dataList}: CarouselProps) => {
+/**/
+export default ({dataList, onPress}: CarouselProps) => {
   return (
     <View style={{height: 200}}>
       <Swiper autoplay={true}>
         {dataList.map(item => (
-          <View
+          <TouchableOpacity
+            onPress={() => onPress(item)}
             key={item.id}
             style={{
               margin: 20,
               flex: 1,
               borderRadius: 10,
               overflow: 'hidden',
-              backgroundColor: 'red',
             }}>
-            <Image
-              source={{uri: item.uri}}
-              style={{flex: 1, height: 170}}
-            />
-          </View>
+            <Image source={{uri: item.uri}} style={{flex: 1, height: 170}} />
+          </TouchableOpacity>
         ))}
       </Swiper>
     </View>
--- src/pages/Discover/index.tsx
+++ src/pages/Discover/index.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import SearchBarHome from '../../components/SearchBarHome';
-import CarouselImg from '../../components/CarouselImg';
+import CarouselImg, {ImgItemType} from '../../components/CarouselImg';
+
 import {ScrollView, Button} from 'react-native';
 import axios from '../../utils/axios';
 import {TabRouterProps} from '../../utils/typeInterface';
@@ -9,6 +10,7 @@ interface AllProps {
 }
 const Tools = {
   CarouselImgDataListInit(focus: AllProps) {
+    console.log(focus);
     return focus.data.content.map((item: AllProps) => {
       return {
         id: item.id,
@@ -19,7 +21,7 @@ const Tools = {
 };
 const Discover = (props: TabRouterProps) => {
   const [musicStore, setMusicStore] = useState<AllProps>({});
-  const [dataList, setDataList] = useState([]);
+  const [dataList, setDataList] = useState<Array<ImgItemType>>([]);
   useEffect(() => {
     axios.get('/getRecommend').then((s: AllProps) => {
       setMusicStore(s);
@@ -31,7 +33,14 @@ const Discover = (props: TabRouterProps) => {
     <>
       <SearchBarHome />
       <ScrollView>
-        {!!dataList.length && <CarouselImg dataList={dataList} />}
+        {!!dataList.length && (
+          <CarouselImg
+            dataList={dataList}
+            onPress={(data: ImgItemType) => {
+              console.log(data);
+            }}
+          />
+        )}
         {/*<VideoPlayer />*/}
         <Button
           title="Go to Details... again"
